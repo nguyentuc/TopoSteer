63ae2a8ed8f5c0110db7bb2c3d4fc1f83a119bbc
nguyentuc <nguyentuc1003@gmail.com>
2026-03-16 20:16:08 +0000

    update

--- LayerNavigator/z8_main_v3_h100_2.sh
+++ LayerNavigator/z8_main_v3_h100_2.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-#SBATCH --account=le-lab
-#SBATCH --gres=gpu:H100:1
-#SBATCH --mem=100GB
-#SBATCH --time=336:00:00
-#SBATCH --partition=general
-#SBATCH --output=z8_main_v3_LLama8B-%j.out
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=tucnguye@iu.edu
-
 nvidia-smi
 eval "$(conda shell.bash hook)"
 conda env list
